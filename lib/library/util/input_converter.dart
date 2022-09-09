ae34ecfa15c2fa205bfc54c38e9bdc52971d1451
Juraj Macak <jurajmacak@gmail.com>
2022-09-09 11:06:01 +0200

    Number Trivia Bloc boilerplate with event and state implementation, input converter

---
+++ lib/library/util/input_converter.dart
@@ -0,0 +1,20 @@
+import 'dart:ffi';
+
+import 'package:dartz/dartz.dart';
+
+import '../error/failure.dart';
+
+class InputConverter {
+  Either<Failure, int> stringToUnsignedInteger(String str) {
+    try {
+      final integer = int.parse(str);
+      if (integer < 0) throw const FormatException();
+
+      return Right(integer);
+    } on FormatException {
+      return Left(InvalidInputFailure());
+    }
+  }
+}
+
+class InvalidInputFailure extends Failure {}
